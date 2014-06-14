74c2d767ba666c266190f3dfc6952be2ca01088d
ostenbom <oliver@stenbom.eu>
2014-06-14 15:00:47 +0200

    started trying to get the JSON to work in articles.html with handlebars. Unsucessful...

--- platforms/android/CordovaLib/ant-gen/R.java.d
+++ platforms/android/CordovaLib/ant-gen/R.java.d
@@ -1,1 +1,1 @@
- : C:\Users\Oliver\Barcelona2014\platforms\android\CordovaLib\ant-build\AndroidManifest.xml \
+ : C:\Users\Oliver\Documents\GitHub\Barcelona2014\platforms\android\CordovaLib\ant-build\AndroidManifest.xml \
